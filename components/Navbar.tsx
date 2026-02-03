d1b07e093b7b5d41642711f7de66c4a47a08ef07
RizkyRR27 <rizkyroza2005@gmail.com>
2026-02-03 13:17:00 +0700

    edit navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,8 +8,8 @@ export default function Navbar() {
       </Link>
       <div className="flex gap-8 font-medium uppercase tracking-widest text-xs">
         <Link href="/" className="hover:text-blue-500 transition-colors">Home</Link>
-        <Link href="/about" className="hover:text-red-500 transition-colors">About</Link>
-        <Link href="/projects" className="hover:text-blue-500 transition-colors">Projects</Link>
+        <Link href="/HUBi" className="hover:text-red-500 transition-colors">Hubungin Gue</Link>
+        {/* <Link href="/projects" className="hover:text-blue-500 transition-colors">Projects</Link> */}
       </div>
     </nav>
   );
